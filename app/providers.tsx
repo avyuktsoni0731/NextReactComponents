99a9abf9653d70e97897646c8c32662ba02e86a0
Avyukt Soni <soniavyukt@gmail.com>
2024-04-08 00:01:20 +0530

    :fire: provider update

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <GoogleOAuthProvider>
         <NextUIProvider>{children}</NextUIProvider>
-      </GoogleOAuthProvider>
     </>
   );
 }
